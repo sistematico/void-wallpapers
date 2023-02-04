1f4967173a5552b4f420e7df2f78df4501a134a0
Lucas Saliés Brum <97509+sistematico@users.noreply.github.com>
2023-02-04 18:49:15 -0400

    Updated paths

--- create-json.sh
+++ create-json.sh
@@ -3,12 +3,28 @@
 OLDPWD="$(pwd)"
 cd public
 
+# identify -format '%wx%h' public/walls/wall-00.png
+
 ARGS=($(\ls -d walls/*))
-printf '%s\n' "${ARGS[@]}" |\
-        jq --slurp -R \
-        'split("\n")[:-1] |
-        to_entries |
-        map({ name: ("/" + .value), index: .key }) |
-        .' > ../json/data.json
+
+i=0
+json=''
+for item in "${ARGS[@]}"
+do
+    ((i+1)) 
+    # json="$(jq --arg val "$i" '.input.names += [$val]' <<< "$json")"
+
+    # jq -sR --arg val "$i" '.input.names += [$val]'
+
+    echo $(jq --arg file "$item" '. += $file')
+
+done
+
+# printf '%s\n' "${ARGS[@]}" |\
+#         jq --slurp -R \
+#         'split("\n")[:-1] |
+#         to_entries |
+#         map({ name: ("/" + .value), index: .key }) |
+#         .' > ../json/data.json
 
 cd "$OLDPWD"
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    base: env.NODE_ENV == 'github' ? '/void-wallpapers/' : '/',
-    plugins: [
-      vue()
-    ]
-  }
+export default defineConfig({
+    plugins: [vue()]
 })
